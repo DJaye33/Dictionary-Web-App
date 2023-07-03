7aaeca6ecb5045011fec7fb12fbb0e31c5ee1296
Danny JeanLouis <dlouis3386@gmail.com>
2023-07-03 16:27:48 -0400

    add app cantainer and app bar with logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { AppBar } from "./Components";
+
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        {/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
+        <div className="mx-auto max-w-3xl">
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
+          <AppBar />
+        </div>
+      </div>
     </>
   );
 }
